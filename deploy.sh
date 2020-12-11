224c1c916f2d08dc2d4b9ee133e75ef746e1fb55
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-11 19:47:08 +0200

    upgraded deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo `date "+%Y-%m-%d %T DEPLOY";git pull;yarn;yarn build` >> deploy.log;
+echo `date "+[%Y-%m-%d %T]";git pull > /dev/null;echo pull=$?;yarn > /dev/null;echo install=$?;yarn build > /dev/null;echo build=$?` >> pages/api/deploy.log;
