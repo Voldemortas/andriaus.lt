f98b0ffaa7413646178a83da1e6fab8b555f80dc
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-11 20:00:14 +0200

    upgraded deploy script

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -1,17 +1,24 @@
 import { execFile } from 'child_process'
+import { readFileSync } from 'fs'
 
 export default async (req, res) => {
-  const now = new Date().getTime()
-  execFile('./deploy.sh', (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`)
-    }
-    console.log(`stdout: ${stdout}`)
+  if (req.method === 'POST') {
+    const now = new Date().getTime()
+    execFile('./deploy.sh', (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+      }
+      console.log(`stdout: ${stdout}`)
 
-    res.statusCode = 200
-    res.json({ duration: new Date().getTime() - now })
-  })
+      res.statusCode = 200
+      res.json({ duration: new Date().getTime() - now })
+    })
+  } else if (req.method === 'GET') {
+    res.send(await readFileSync('./pages/api/deploy.log', 'utf-8'))
+  }
+  res.statusCode = 400
+  res.json({ duration: new Date().getTime() - now })
 }
