6625dce6d7e16ea023181e00b5b1acff2b62dd9e
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-24 07:54:22 +0200

    removed unused links and changed gifs to videos

--- components/SliderArticle.tsx
+++ components/SliderArticle.tsx
@@ -1,7 +1,7 @@
 import React, { ReactChild } from 'react'
 type properties = {
   title: string
-  url: string
+  url: string | null
   children: ReactChild
   image: string
   repo: string
@@ -13,18 +13,45 @@ const SliderArticle = ({ title, url, children, image, repo }: properties) => {
       <header>
         <h2>{title}</h2>
       </header>
-      <a href={image} className="image-url">
-        <img
-          src={image}
-          width="320"
-          alt={`preview of ${title}`}
-          title={`preview of ${title}`}
-        ></img>
-      </a>
+      <span className="image-url">
+        {!/\.gif$/.test(image) ? (
+          <>
+            <img
+              src={image}
+              width="320"
+              alt={`preview of ${title}`}
+              title={`preview of ${title}`}
+            />
+          </>
+        ) : (
+          <video autoPlay={true} loop muted playsInline>
+            <source
+              src={`${image.match(/(.+)\.gif$/)[1]}.webm`}
+              type="video/webm"
+            />
+            <source
+              src={`${image.match(/(.+)\.gif$/)[1]}.mp4`}
+              type="video/mp4"
+            />
+            <img
+              src={image}
+              width="320"
+              alt={`preview of ${title}`}
+              title={`preview of ${title}`}
+            />
+          </video>
+        )}
+      </span>
       <article>{children}</article>
       <br />
       <footer>
-        Live page: <a href={url}>{url}</a>
+        {url === null || url === '' || url === undefined ? (
+          <></>
+        ) : (
+          <>
+            Live page: <a href={url}>{url}</a>
+          </>
+        )}
         <br />
         Github repo: <a href={repo}>{repo}</a>
       </footer>
