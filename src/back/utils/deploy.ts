32095fd0f9d2002c75f2db2c8a73750caacfcef3
Andrius Simanaitis <eragonas5@gmail.com>
2025-07-06 21:41:27 +0300

    update deploy script

--- src/back/utils/deploy.ts
+++ src/back/utils/deploy.ts
@@ -2,6 +2,7 @@ import {$} from 'bun'
 import getTimestamp from 'back/common/timestamp.ts'
 
 export default async function deploy() {
+  await $`echo '${getTimestamp()} Trying to redeploy!' >> logs/deploy.log`.nothrow().quiet()
   const pull = await $`git pull`.nothrow().quiet()
   if (pull.exitCode !== 0) {
     await $`echo '${getTimestamp()} pull: ${pull.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
@@ -17,11 +18,6 @@ export default async function deploy() {
     await $`echo '${getTimestamp()} build: ${build.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
     return build
   }
-  const pm2 = await $`bun run serve-pm2`.nothrow().quiet()
-  if (pm2.exitCode !== 0) {
-    await $`echo '${getTimestamp()} pm2: ${pm2.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
-    return pm2
-  }
-  await $`echo '${getTimestamp()} success: deployed!' >> logs/deploy.log`.nothrow().quiet()
-  return pm2
+  await $`echo '${getTimestamp()} Restarting pm2' >> logs/deploy.log`.nothrow().quiet()
+  return await $`bun run serve-pm2`.nothrow().quiet()
 }
