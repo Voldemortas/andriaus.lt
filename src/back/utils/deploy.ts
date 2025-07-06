fbfeebcf121ec3700c224a78462c4f578b595363
Andrius Simanaitis <eragonas5@gmail.com>
2025-07-06 20:35:15 +0300

    update deploy script

--- src/back/utils/deploy.ts
+++ src/back/utils/deploy.ts
@@ -3,22 +3,22 @@ import getTimestamp from 'back/common/timestamp.ts'
 
 export default async function deploy() {
   const pull = await $`git pull`.nothrow().quiet()
-  if (pull.stderr) {
+  if (pull.exitCode !== 0) {
     await $`echo '${getTimestamp()} pull: ${pull.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
     return pull
   }
   const install = await $`bun install`.nothrow().quiet()
-  if (install.stderr) {
+  if (install.exitCode !== 0) {
     await $`echo '${getTimestamp()} install: ${install.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
     return pull
   }
   const build = await $`bun run build-prod`.nothrow().quiet()
-  if (pull.stderr) {
+  if (pull.exitCode !== 0) {
     await $`echo '${getTimestamp()} build: ${build.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
     return build
   }
   const pm2 = await $`bun run serve-pm2`.nothrow().quiet()
-  if (pm2.stderr) {
+  if (pm2.exitCode !== 0) {
     await $`echo '${getTimestamp()} pm2: ${pm2.stderr.toString()}' >> logs/deploy.log`.nothrow().quiet()
     return pm2
   }
